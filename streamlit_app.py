a507707cbbad735b9195aad1dada7bf67aa95f53
hurmade-harsha <123945524+hurmade-harsha@users.noreply.github.com>
2023-07-21 15:46:56 +0530

    Update streamlit_app.py
    add several images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('🥣 Omega 3 and blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 avocado, toast')
+
+
+
 
