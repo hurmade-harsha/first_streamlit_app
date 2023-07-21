d09ca8083e5affd288f19c51f199a4ce679fc502
hurmade-harsha <123945524+hurmade-harsha@users.noreply.github.com>
2023-07-21 15:57:35 +0530

    Update streamlit_app.py
    panda function called and read csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 and blueberry oatmeal')
@@ -8,5 +9,7 @@ streamlit.text('🥑🍞 avocado, toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
