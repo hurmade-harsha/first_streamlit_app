400fd6c5ef8ef204589db3f5309188b909b6ee6b
hurmade-harsha <123945524+hurmade-harsha@users.noreply.github.com>
2023-07-21 15:48:34 +0530

    Update streamlit_app.py
    add more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.text('🥣 Omega 3 and blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 avocado, toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
